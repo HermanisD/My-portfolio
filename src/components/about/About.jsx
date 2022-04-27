7d7398f940c130eafc5506c0457546c50f96a156
Hermanis Djomkins <hermanis.djomkins@gmail.com>
2022-04-27 09:16:10 +0300

    added experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,21 +23,21 @@ const About = () => {
             <article className='about_card'>
               <FaAward className="about_icon" />
               <h5>Expierence</h5>
-              <small>3+ Years Working</small>
+              <small>1 Year Learning</small>
             </article>
             <article className='about_card'>
               <FiUsers className="about_icon" />
-              <h5>Clients</h5>
+              <h5>Potential Clients</h5>
               <small>200+ Worldwide</small>
             </article>
             <article className='about_card'>
               <VscFolderLibrary className="about_icon" />
-              <h5>Projects</h5>
-              <small>80+ Completed</small>
+              <h5>Pet Projects</h5>
+              <small>20+ Completed</small>
             </article>
           </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui consectetur esse magnam saepe. Dolore itaque perferendis nesciunt qui, deserunt accusantium dolor ea nihil perspiciatis, recusandae at aut repellat natus error.
+          For the last 6 months I have been intensively learning the basics of programming both in the form of self-study and various courses. The basics of HTML, CSS and JavaScript are learned. Performed many different types of tasks in Javascript, and also worked on the development of website templates. The basics of both Git and GitHub are known, as well as usage of Bootstrap. Applications and samples can be viewed in my GitHub repositories: <a href = "https://github.com/HermanisD">GitHub</a>. My CV as a web-page is also available here: <a href="https://hermanisd.github.io/CV-HTML-CSS-JS/">Curriculum Vitae</a>. I am currently continuing to move towards REACT. I have also started learning Python. Given that theoretical knowledge without practice is not sustainable, I am interested in finding an opportunity to use my knowledge and grow in the direction of a programming specialist. I very much hope and believe that I can be useful to you.
           </p>
           <a href='#contact' className='btn btn-primary'>Let's Talk</a>
         </div>
